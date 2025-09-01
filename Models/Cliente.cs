d59f57f6acf4f1b5c637a61735aa50159265939c
Luigi <159388072+LuigiBerzaghi@users.noreply.github.com>
2025-09-01 15:41:05 -0300

    Corrigindo under-posting

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -1,24 +1,22 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Sprint1CSharp.Models
 {
     public class Cliente
     {
         [Key]
-        public int Id { get; set; }
+        [BindRequired]              
+        [Range(1, int.MaxValue)]
+        public int Id { get; set; } 
 
-        [Required]
-        public string? Nome { get; set; }
-
-        [Required]
-        public string? CPF { get; set; }
-
-        [Required]
-        public string? Email { get; set; }
+        [Required] public string? Nome { get; set; }
+        [Required] public string? CPF  { get; set; }
+        [Required, EmailAddress] public string? Email { get; set; }
 
         public string? Endereco { get; set; }
-
         public List<Veiculo> Veiculos { get; set; } = new();
     }
 }
